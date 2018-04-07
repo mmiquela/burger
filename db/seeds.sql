7c8c19ab3f5148bb972d13985aaf72cdc836eddd
Marsha Miquela <marshamiquela@Virgilios-MBP.home>
2018-04-06 21:50:45 -0400

    Feat: Burger Logger with MySQL, Node, Express, Handlebars and ORM

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Seed Burgers into Database ... No Sesame! (hehehe)
+INSERT INTO burgers
+    (burger_name, devoured, date)
+VALUES
+    ("Veggie", false, "2018-02-05 04:20:00"),
+    ("Chicken Salad", false, "2018-02-05 04:20:01"),
+    ("Cheeseburger", false, "2018-02-05 04:20:02"),
+    ("Tuna Salad", false, "2018-02-05 16:20:00"),
+    ("Quarter Pounder", false, "2018-02-05 16:20:01"),
+    ("Salmon Burger", false, "2018-02-05 04:20:20"),
+    ("Grilled Mushroom Burger", true, "2018-02-05 16:20:16");
